ea20d2e1c4bf848aa4b8caffe65bf5a21c325f9c
SamHFarrell <119333107+SamHFarrell@users.noreply.github.com>
2022-12-01 17:51:10 +0000

    Initial commit of IDM1

---
+++ labs/IDM1/model_function_code.R
@@ -0,0 +1,10 @@
+# This mostly blank .R file has been provided so you can keep your model code, and any other useful bits of code you write, in here.
+# Let us know if you have any questions as you go along!
+
+# Most of your code will start with 
+
+library("deSolve")
+library("ggplot2")
+
+
+## YOUR CODE HERE ##
